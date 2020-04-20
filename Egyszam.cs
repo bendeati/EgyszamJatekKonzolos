4a478bbbd1b80802fdf84922ed3f62163fb60ace
Bende Attila <47759047+bendeati@users.noreply.github.com>
2020-04-20 11:58:46 +0200

    Add files via upload

---
+++ Egyszam.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgyszamMinta2020
+{
+    class Egyszam
+    {
+        private string nev;
+        //private int elso;
+        //private int masodik;
+        //private int harmadik;
+        //private int negyedik;
+        List<int> fordulok = new List<int>();
+
+        public Egyszam(string sor)
+        {
+            string[] d = sor.Split(' ');
+            nev = d[0];
+            for (int i = 1; i < d.Length; i++)
+            {
+                fordulok.Add(Convert.ToInt32(d[i]));
+            }
+            
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public List<int> Fordulok { get => fordulok; set => fordulok = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EgyszamMinta2020
+{
+    class Program
+    {
+        static List<Egyszam> egyszamLista = new List<Egyszam>();
+
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("egyszamjatek1.txt", Encoding.UTF8);
+            string sor = "";
+            while (!sr.EndOfStream)
+            {
+                sor = sr.ReadLine();
+                Egyszam e = new Egyszam(sor);
+                egyszamLista.Add(e);
+            }
+            sr.Close();
+
+            Console.WriteLine("3. feladat");
+            Console.WriteLine("Játékosok száma: " + egyszamLista.Count);
+
+            Console.WriteLine("4. feladat");
+            Console.Write("Kérem a forduló sorszámát: ");
+            int sorszam = Convert.ToInt32(Console.ReadLine());
+            int tippOsszeg = 0;
+            for (int i = 0; i < egyszamLista.Count; i++)
+            {
+                tippOsszeg += egyszamLista[i].Fordulok[sorszam - 1];
+            }
+            double tippAtlag = (double)tippOsszeg / egyszamLista.Count;
+            Console.WriteLine("5. feladat");
+            Console.WriteLine("A megadott forduló tippjeinek átlaga: " 
+                + Math.Round(tippAtlag,2));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
